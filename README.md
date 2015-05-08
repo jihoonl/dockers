142a91cbd4ec41e9f2ee93cc62fff13add72d32d
Jihoon Lee <jihoonlee.in@gmail.com>
2015-05-08 10:00:07 +0900

    updates

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # dockers
+
+Inspired by https://github.com/creativa77/rocon_web_proxy_files
+Thanks to Julian Cerruti
---
+++ turtlebot_web_view/build_environment.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+# Build a Docker iamge from a Docker file
+sudo docker build -t jihoonl/turtlebot-web-view .
